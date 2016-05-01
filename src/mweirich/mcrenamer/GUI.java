8b53cd80e218af9245e88c943941571af4ba4621
vavido <matz.weirich@gmail.com>
2016-05-01 18:25:12 +0200

    initial commit

---
+++ src/mweirich/mcrenamer/GUI.java
@@ -0,0 +1,11 @@
+package mweirich.mcrenamer;
+
+import javax.swing.JFrame;
+
+public class GUI {
+	
+	public GUI(){
+		JFrame frame = new JFrame("MC Renamer");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
